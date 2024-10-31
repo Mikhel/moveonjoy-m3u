8f399c5727190077aa4d3993b3d908d35954d8b7
Mikhel <mbarajas@gmail.com>
2024-10-31 13:20:29 -0700

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -53,11 +53,10 @@ def filter_and_build_epg(urls):
     print(f"New EPG saved to {output_file}")
 
 urls = [
+    'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_MX1.xml.gz',
-    'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
-    'http://m3u4u.com/epg/jq2zy98d6es52d1knxr5',
     'https://epgshare01.online/epgshare01/epg_ripper_CL1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CO1.xml.gz',
 ]
