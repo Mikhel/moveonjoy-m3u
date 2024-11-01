4c6cc0af22720009ccd8198671dd888d75b547a0
Mikhel <mbarajas@gmail.com>
2024-11-01 00:36:03 -0700

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -64,7 +64,6 @@ urls = [
 if __name__ == "__main__":
     filter_and_build_epg(urls)
 
-def compressing the xml file:
 f_in = open('/EPG-para-m3u/epg.xml')
 f_out = gzip.open('/EPG-para-m3u/epg.xml.gz', 'wb')
 f_out.writelines(f_in)
